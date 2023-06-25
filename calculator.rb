e140622f6a8cf4ee0d0607d4d727458265bb6778
Alex <alexkimanthi92@gmail.com>
2023-06-25 11:23:04 +0300

    update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,15 @@ def calculator
 
     puts "Please enter the second number:"
     num2 = gets.chomp.to_f
+
+    puts "Select an operation:"
+    puts "1. Addition"
+    puts "2. Subtraction"
+    puts "3. Multiplication"
+    puts "4. Division"
+    operation = gets.chomp.to_i
+  
   
 end
+
+calculator
