3296cd57bbf315a5a8797e8d43461582324783e3
Alex <alexkimanthi92@gmail.com>
2023-06-25 11:24:17 +0300

    update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -12,7 +12,31 @@ def calculator
     puts "3. Multiplication"
     puts "4. Division"
     operation = gets.chomp.to_i
+
+    case operation
+    when 1
+      result = num1 + num2
+      operator = "+"
+    when 2
+      result = num1 - num2
+      operator = "-"
+    when 3
+      result = num1 * num2
+      operator = "*"
+    when 4
+      if num2 == 0
+        puts "Error: Division by zero!"
+        return
+      end
+      result = num1 / num2
+      operator = "/"
+    else
+      puts "Invalid operation!"
+      return
+    end
   
+    puts "#{num1} #{operator} #{num2} = #{result}"
+  end
   
 end
 
