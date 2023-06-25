7126f95d89a522abba9f5608b9a2aa6a54317a46
Alex <alexkimanthi92@gmail.com>
2023-06-25 11:20:20 +0300

    update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,8 @@ def calculator
     puts "Welcome to the Calculator App!"
     puts "Please enter the first number:"
     num1 = gets.chomp.to_f
+
+    puts "Please enter the second number:"
+    num2 = gets.chomp.to_f
   
 end
